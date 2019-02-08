bfd59d7cf668bc72c73959bcbc935a3082a42f0e
Nicole D <nnardella@gmail.com>
2019-02-08 01:44:11 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -10,11 +10,12 @@ gem 'rake'
 gem 'require_all'
 gem 'shotgun'
 gem 'json'
+gem 'pry-nav'
 
 group :test do
   gem 'rspec'
   gem 'pry'
-  gem 'pry-nav'
+
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes' do
     @recipes = Recipe.all
+
     erb :index
   end
 
@@ -17,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     
   end
 
